4384645ee39642d41b3961c29715a36cec0987c4
Nevercome <sunss1903@163.com>
2020-02-03 20:26:55 +0800

    max heap insert

--- Basic/src/heap/MaxHeap.java
+++ Basic/src/heap/MaxHeap.java
@@ -8,8 +8,28 @@ package heap;
  */
 public class MaxHeap {
 
-    public void init(int[] heap) {
-        print(heap);
+    private int[] heap;
+    private int maxSize = -1;
+    private int heapSize = 0;
+
+    private final int DEFAULT_HEAP_SIZE = 100;
+
+    public MaxHeap(int[] heap) {
+        if(this.maxSize == -1) maxSize = DEFAULT_HEAP_SIZE;
+        this.heap = new int[maxSize];
+        System.arraycopy(heap, 0, this.heap, 1, heap.length);
+        heapSize = heap.length;
+    }
+
+    public MaxHeap(int[] heap, int maxSize) {
+        this.maxSize = maxSize;
+        this.heap = new int[maxSize];
+        System.arraycopy(heap, 0, this.heap, 1, heap.length);
+        heapSize = heap.length;
+    }
+
+    public void init() {
+        print();
         int size = heap.length;
         for (int i = size / 2; i >= 1; i--) {
             // 存储当前节点
@@ -26,28 +46,50 @@ public class MaxHeap {
                     break;
                 } else {
                     // 自己和子节点交换 这里不能使用i 要使用left / 2
-                    heap[left/2] = heap[left];
+                    heap[left / 2] = heap[left];
                     heap[left] = temp;
                     // 自己调整到子节点的位置 继续向下比较 直到这个堆调整完成
                     left *= 2;
                 }
             }
         }
-        print(heap);
+        print();
     }
 
+    public void insert(int node) {
+        // 插入节点到最大堆
+        if (heapSize >= maxSize) {
+            System.out.println("out of size");
+            return;
+        }
+        // 插入到最后
+        heap[heapSize + 1] = node;
+        heapSize++;
+        // 循环向父节点比较交换 直到顶
+        for (int i = heapSize; i > 1; i /= 2) {
+            if (heap[i / 2] < heap[i]) {
+                int temp = heap[i];
+                heap[i] = heap[i / 2];
+                heap[i / 2] = temp;
+            } else {
+                break;
+            }
+        }
+    }
 
-    private void print(int[] arr) {
-        for (int a : arr) {
-            System.out.print(a + " ");
+    private void print() {
+        for (int i = 1; i <= heapSize; i++) {
+            System.out.print(heap[i] + " ");
         }
         System.out.println();
     }
 
     public static void main(String[] args) {
-        MaxHeap maxHeap = new MaxHeap();
-        int[] arr = {-1, 4, 1, 3, 2, 16, 9, 10, 14, 8, 7};
-        maxHeap.init(arr);
+        int arr[] = {4, 1, 3, 2, 16, 9, 10, 14, 8, 7};
+        MaxHeap maxHeap = new MaxHeap(arr);
+        maxHeap.init();
+        maxHeap.insert(13);
+        maxHeap.print();
     }
 
 }
