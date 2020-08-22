00ca2d6abf126ae9c7bce9aed044b0c11a7f5c1a
Nevercome <sunss1903@163.com>
2020-08-22 20:40:25 +0800

    14 max common prefix

---
+++ 14_MaxCommonPrefix/src/Solution.java
@@ -0,0 +1,32 @@
+/**
+ * 题目：最长公共前缀
+ * 思路：前缀在每次比较后只会减少，只需遍历一遍数组即可
+ * 每个字符串的长度也算入时间复杂度
+ * @author Sunss
+ * @since 2020/8/22
+ */
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        String prefix = strs[0];
+        int count = strs.length;
+        for (int i = 1; i < count; i++) {
+            prefix = longestCommonPrefix(prefix, strs[i]);
+            if (prefix.length() == 0) {
+                break;
+            }
+        }
+        return prefix;
+    }
+
+    public String longestCommonPrefix(String str1, String str2) {
+        int length = Math.min(str1.length(), str2.length());
+        int index = 0;
+        while (index < length && str1.charAt(index) == str2.charAt(index)) {
+            index++;
+        }
+        return str1.substring(0, index);
+    }
+}
