890c6d75f97326af07cd080189da987513448bf9
Nevercome <sunss1903@163.com>
2020-08-30 10:20:51 +0800

    557 ReverseWords

---
+++ 557_ReverseWords/src/Solution.java
@@ -0,0 +1,27 @@
+/**
+ * 题目：反转字符串中的单词
+ * 思路：从尾向前遍历，遇到空格输出
+ * @author Sunss
+ * @since 2020/8/30
+ */
+class Solution {
+    public String reverseWords(String s) {
+        StringBuffer ret = new StringBuffer();
+        int length = s.length();
+        int i = 0;
+        while (i < length) {
+            int start = i;
+            while (i < length && s.charAt(i) != ' ') {
+                i++;
+            }
+            for (int p = start; p < i; p++) {
+                ret.append(s.charAt(start + i - 1 - p));
+            }
+            while (i < length && s.charAt(i) == ' ') {
+                i++;
+                ret.append(' ');
+            }
+        }
+        return ret.toString();
+    }
+}
