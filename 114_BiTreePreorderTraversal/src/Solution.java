54f73f1db2a4f66aeaa157d5e63735c0cebebf13
Nevercome <sunss1903@163.com>
2020-09-14 23:48:05 +0800

    114 bi tree postorder traversal

---
+++ 114_BiTreePreorderTraversal/src/Solution.java
@@ -0,0 +1,72 @@
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 题目：二叉树的前序遍历
+ * @author Sunss
+ * @since 2020/9/14
+ */
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        this.val = x;
+    }
+}
+
+public class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        LinkedList<Integer> output = new LinkedList<>();
+        if (root == null) {
+            return output;
+        }
+
+        stack.add(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pollLast();
+            output.add(node.val);
+            if (node.right != null) {
+                stack.add(node.right);
+            }
+            if (node.left != null) {
+                stack.add(node.left);
+            }
+        }
+        return output;
+    }
+
+    public List<Integer> preorderTraversal_morris(TreeNode root) {
+        LinkedList<Integer> output = new LinkedList<>();
+
+        TreeNode node = root;
+        while (node != null) {
+            if (node.left == null) {
+                output.add(node.val);
+                node = node.right;
+            }
+            else {
+                TreeNode predecessor = node.left;
+                while ((predecessor.right != null) && (predecessor.right != node)) {
+                    predecessor = predecessor.right;
+                }
+
+                if (predecessor.right == null) {
+                    output.add(node.val);
+                    predecessor.right = node;
+                    node = node.left;
+                }
+                else{
+                    predecessor.right = null;
+                    node = node.right;
+                }
+            }
+        }
+        return output;
+    }
+
+
+}
