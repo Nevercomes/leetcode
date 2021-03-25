8e4c8316b09028b8e2e9ff9b86c8befa55e2df2b
Nevercome <sunss1903@163.com>
2021-03-25 14:16:48 +0800

    232 Implement Queue using Stacks

---
+++ 232_ImplementQueueUsingStacks/src/MyQueue.java
@@ -0,0 +1,56 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 用两个栈来模拟队列
+ * 时间复杂度 pop和peek为均摊 O(1)
+ *
+ * @author Sunss
+ * @since 2021/3/25
+ */
+class MyQueue {
+    Deque<Integer> inStack;
+    Deque<Integer> outStack;
+
+    public MyQueue() {
+        inStack = new LinkedList<>();
+        outStack = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        inStack.push(x);
+    }
+
+    public int pop() {
+        if (outStack.isEmpty()) {
+            in2out();
+        }
+        return outStack.pop();
+    }
+
+    public int peek() {
+        if (outStack.isEmpty()) {
+            in2out();
+        }
+        return outStack.peek();
+    }
+
+    public boolean empty() {
+        return inStack.isEmpty() && outStack.isEmpty();
+    }
+
+    private void in2out() {
+        while (!inStack.isEmpty()) {
+            outStack.push(inStack.pop());
+        }
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
