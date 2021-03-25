b98ed2880bfe9189af3fcfe22bce0c1219b6dfb6
Nevercome <sunss1903@163.com>
2021-03-25 16:05:14 +0800

    682 baseball game

---
+++ 682_BaseballGame/src/Solution.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+/**
+ * @author Sunss
+ * @since 2021/3/25
+ */
+class Solution {
+    public int calPoints(String[] ops) {
+        Stack<Integer> stack = new Stack();
+
+        for(String op : ops) {
+            if (op.equals("+")) {
+                int top = stack.pop();
+                int newTop = top + stack.peek();
+                stack.push(top);
+                stack.push(newTop);
+            } else if (op.equals("C")) {
+                stack.pop();
+            } else if (op.equals("D")) {
+                stack.push(2 * stack.peek());
+            } else {
+                stack.push(Integer.valueOf(op));
+            }
+        }
+
+        int ans = 0;
+        for(int score : stack) ans += score;
+        return ans;
+    }
+}
