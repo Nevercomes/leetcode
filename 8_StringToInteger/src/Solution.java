8c2d51d2ac720550a019adf82d76e984b5044fdf
Nevercome <sunss1903@163.com>
2021-04-03 09:31:41 +0800

    8 跟踪调试 DFS

--- 8_StringToInteger/src/Solution.java
+++ 8_StringToInteger/src/Solution.java
@@ -22,7 +22,7 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        solution.myAtoi("-91283472332");
+        solution.myAtoi(" -91 W");
     }
 
 }
